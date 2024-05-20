c4bfb521b2e42ebdbcdfa9528d2f8f5c352d9a17
student <kmac.kimaru93@gmail.com>
2024-05-20 05:38:06 +0300

    adding functions in python lab

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,20 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+    return int(num1) + int(num2)
 
 def halve(number):
     pass
+    return float(number) / 2
